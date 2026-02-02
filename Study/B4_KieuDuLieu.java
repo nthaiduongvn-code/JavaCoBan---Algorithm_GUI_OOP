86cfb4f7c8ddf1e1f869e62c563f993736824624
nthaiduongvn-code <n.thaiduong.vn@gmail.com>
2026-02-02 10:07:41 +0700

    Delete Study directory

--- Study/B4_KieuDuLieu.java
+++
@@ -1,8 +0,0 @@
-package Study;
-
-public class B4_KieuDuLieu {
-    static void main() {
-        System.out.println( "kiểu dữ diệu integer Min: "+ Integer.MIN_VALUE);
-        System.out.println( "kiểu dữ diệu integer Max: "+ Integer.MAX_VALUE);
-    }
-}
